dd0f128a4415e8a71d6f6b081af7b041d2031515
kyro04-dev <kyrodev23@gmail.com>
2026-02-09 23:19:52 +0800

    Update script.js

--- modules/script.js
+++ modules/script.js
@@ -1,24 +1,26 @@
 fetch("modules.json")
-  .then(r => r.json())
+  .then(res => res.json())
   .then(data => {
     const list = document.getElementById("module-list");
 
     data.forEach(m => {
-      const item = document.createElement("div");
-      item.className = "module-card";
-      item.onclick = () => {
+      const card = document.createElement("div");
+      card.className = "module-card";
+      card.onclick = () => {
         location.href = `module.html?id=${m.id}`;
       };
 
-      item.innerHTML = `
-        <img src="${m.image_url}">
-        <div>
+      card.innerHTML = `
+        <img class="card-image" src="${m.image_url}">
+        <div class="card-content">
           <h2>${m.name}</h2>
+          <p class="meta">
+            v${m.version} • SDK ≥ ${m.min_sdk}
+          </p>
           <span class="badge">${m.action_type.toUpperCase()}</span>
-          <p>Version ${m.version} • SDK ≥ ${m.min_sdk}</p>
         </div>
       `;
 
-      list.appendChild(item);
+      list.appendChild(card);
     });
   });
